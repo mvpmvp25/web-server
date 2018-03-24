26a26a5398eec7727d26617d22e0f2c210338517
fizz <441>
2018-03-24 22:21:01 +0800

    新建项目

--- dev-server.js
+++ dev-server.js
@@ -3,15 +3,30 @@ var app = express();
 
 app.use(express.static('dist')); // server目录(访问静态资源文件)
  
+/*
+app.get('/', function (req, res) {
+   res.sendFile( __dirname + "/dist/index.html" );
+});
+*/
+
+app.get('/login', function (req, res) {
+   res.sendFile( __dirname + "/dist/index.html" );
+});
+
+app.get('/personal', function (req, res) {
+   res.sendFile( __dirname + "/dist/index.html" );
+});
+
 var server = app.listen(8060, function () {
  
   //var host = server.address().address
   var port = server.address().port
   
-  console.log('Server running at http://127.0.0.1:' + port); 
-   
+  console.log('Server running at http://127.0.0.1:' + port);
+  
   // 运行URL
   // http://127.0.0.1:8060
-  // http://127.0.0.1:8060/index.html
+  // http://127.0.0.1:8060/login
+  // http://127.0.0.1:8060/personal
  
 })
