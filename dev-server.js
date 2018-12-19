7a696e1b0aef3095817a490eff140b3b94524480
huangganghengdd01 <ganghenghuang@hk01.com>
2018-12-19 13:28:05 +0800

    修改所有路由都访问index.html

--- dev-server.js
+++ dev-server.js
@@ -5,20 +5,21 @@ var router = express.Router();
 app.use(express.static('dist')); // server目录(访问静态资源文件)
 
 // 后面加的代码 开始
-app.get('*', function(req, res){ // 404重定向到首页
-  res.redirect('/');
+app.get('*', function (req, res) {
+  // res.redirect('/');
+  res.sendfile('./dist/index.html'); // 所有路由都是访问index.html
 });
 // 后面加的代码 结束
 
 var server = app.listen(8060, function () {
- 
+
   //var host = server.address().address
   var port = server.address().port
-  
-  console.log('Server running at http://127.0.0.1:' + port); 
-   
+
+  console.log('Server running at http://127.0.0.1:' + port);
+
   // 运行URL
   // http://127.0.0.1:8060
   // http://127.0.0.1:8060/index.html
- 
+
 })
---
+++ dist/js/5-899da564.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{223:function(e,n,t){"use strict";Object.defineProperty(n,"__esModule",{value:!0}),n.default={namespace:"products",state:[{key:"1",id:1,name:"dva"},{key:"2",id:2,name:"antd"}],reducers:{delete:function(e,n){var t=n.payload;return e.filter(function(e){return e.id!==t})}}}}}]);
